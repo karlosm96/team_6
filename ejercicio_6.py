b18ca05fff2a9d16cb0f4b30d1034d2ff7ad7c66
Francisco Javier López García Mayorga <javier6282@gmail.com>
2021-11-21 23:34:49 -0300

    last commit

--- ejercicio_6.py
+++ ejercicio_6.py
@@ -1,25 +1,25 @@
 #Crear una clase que represente un numero complejo. Tenga 4 metodos que permita las operaciones matematicas basicas
 #(suma, resta, multiplicacion y division).
 
-class NumeroComplejo(object):
+class NumeroComplejo:
 
-    # Constructor
+    # Constructor de la clase
     def __init__(self, numeroReal, numeroImaginario):
         self.parteReal = numeroReal
         self.parteImaginario = numeroImaginario
 
-    # Metodo para sumar un numero complejo
+    # Metodo para sumar 
     def suma(self, numeroComplejo):
         return (self.parteReal + numeroComplejo.parteReal), (self.parteImaginario + numeroComplejo.parteImaginario)
 
-    # Metodo para restar un numero complejo
+    # Metodo para restar 
     def resta(self, numeroComplejo):
         return (self.parteReal - numeroComplejo.parteReal), (self.parteImaginario - numeroComplejo.parteImaginario)
 
-    # Metodo para multiplicar un numero complejo
+    # Metodo para multiplicar 
     def multiplicacion(self, numeroComplejo):
         return (self.parteReal * numeroComplejo.parteReal) - (self.parteImaginario * numeroComplejo.parteImaginario), (self.parteReal * numeroComplejo.parteImaginario) + (self.parteImaginario * numeroComplejo.parteReal)
 
-    # Metodo para dividir un numero complejo
+    # Metodo para dividir 
     def division(self, numeroComplejo):
         return ((self.parteReal * numeroComplejo.parteReal) + (self.parteImaginario * numeroComplejo.parteImaginario)) / ((numeroComplejo.parteReal * numeroComplejo.parteReal) + (numeroComplejo.parteImaginario * numeroComplejo.parteImaginario)), ((self.parteImaginario * numeroComplejo.parteReal) - (self.parteReal * numeroComplejo.parteImaginario)) / ((numeroComplejo.parteReal * numeroComplejo.parteReal) + (numeroComplejo.parteImaginario * numeroComplejo.parteImaginario))
